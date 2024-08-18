11aecc18a690ad5d7e1cf0274d9ed6f282ea2d04
websitedeb <139923826+websitedeb@users.noreply.github.com>
2024-08-18 01:05:30 +0000

    added use_query hook, along with others

---
+++ app/public/special/hooks/query.py
@@ -0,0 +1,36 @@
+global data, isFetching
+data = {}
+isFetching = False
+
+
+def use_query(key, query):
+  i = 0
+  status = 'error'
+  res = query()
+  while res == None:
+    isFetching = True
+    status = False
+    i += 1
+    if i == 3:
+      status = 'error'
+      isFetching = False
+      break
+  else:
+    status = 'success'
+    isFetching = False
+    data.update({f"""{key}""": f"""{res}"""})
+  return res, status
+
+
+def use_mutation(key, mutation):
+  res = data[f"""{key}"""]
+  if res != None:
+    ret = mutation(res)
+    res = ret
+  else:
+    isFetching = False
+    return False
+
+
+def isFetched():
+  return isFetching
