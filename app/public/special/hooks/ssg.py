b2f578bc63fc7f536bf5a64d5f2762c2f5eed2d8
Sarthak Ghoshal <139923826+websitedeb@users.noreply.github.com>
2024-08-27 17:40:25 +0000

    Added url and string ssg

--- app/public/special/hooks/ssg.py
+++ app/public/special/hooks/ssg.py
@@ -1,7 +1,15 @@
 from markdown_it import MarkdownIt
+from markdown import markdown
 
 
-def use_ssg(loc):
+def use_ssg_file(loc):
   with open(f'../views/{loc}.md', "r") as markdown:
-    md = MarkdownIt("commonmark", {'html': True, 'breaks': True})
-    return md.render(markdown.read())
+    if markdown.read() != "":
+      md = MarkdownIt("commonmark", {'html': True, 'breaks': True})
+      return md.render(markdown.read())
+    else:
+      return ""
+
+
+def use_ssg_string(string):
+  return markdown(string)
