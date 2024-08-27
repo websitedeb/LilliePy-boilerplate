53cc6052021691139b4f607e0aa7ceb8499193ac
Sarthak Ghoshal <139923826+websitedeb@users.noreply.github.com>
2024-08-27 20:16:07 +0000

    Working on it

--- app/public/special/test/test.py
+++ app/public/special/test/test.py
@@ -1,8 +1,25 @@
-from selenium import *
-from flask import Flask
-from reactpy import component
+from flask import Flask, render_template_string, request
+from selenium import webdriver
 
 
-def render(comp):
-  app = Flask(__name__)
-  app.run(debug=True, port=1000)
+def renderer(comp):
+  pass
+
+
+class Actions:
+  pass
+
+
+class Screen:
+  pass
+
+
+'''
+  PROCESS:
+  1) first will take component into renderer
+  2) then will host said component in a different port with a template_string,
+  meaning requires a different flask server
+  3) if error then it returns a error
+  4) actions will be able to interact with the component (buttons, inputs)
+  5)  screen will get or could set anything on the screen (id, class)
+'''
