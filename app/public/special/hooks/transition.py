65d722de3d016efc1a5e4ddcfb51610216dd4354
Sarthak Ghoshal <139923826+websitedeb@users.noreply.github.com>
2024-09-20 00:17:46 +0000

    Added rudimentary version of useTransition

---
+++ app/public/special/hooks/transition.py
@@ -0,0 +1,11 @@
+from reactpy import use_state
+
+def use_transition():
+    is_pending, set_is_pending = use_state(False)
+
+    def start_transition(callback):
+        set_is_pending(True)
+        callback()
+        set_is_pending(False)
+
+    return is_pending, start_transition
